4fb6bd9a61e65af1346bd83b64b8f0c8118cfd53
Yan Liu <yliu3864@uwo.ca>
2021-01-18 04:45:39 -0500

    4-6 done

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['yan@local.dev',Validators.compose([Validators.required, Validators.email, this.validate])],
+      password: ['',Validators.required]
+    })
+   
   }
 
+  onSubmit({value, valid},ev: Event){ 
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(valid);
+  }
+
+  validate(c: FormControl): {[key: string]: any} {
+    if(!c.value){
+      return null;
+    }
+    const pattern = /^yan+/;
+    if(pattern.test(c.value)){
+      return null;
+    }
+    return{
+      emailNotValid: 'the email must start with Yan'
+    }
+  }
 }
